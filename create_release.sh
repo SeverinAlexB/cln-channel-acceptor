55cc7c7a830a294ec9d10696d7bfb129c918f96c
Severin Buhler <sevi_buehler@hotmail.com>
2022-09-30 17:21:56 +0300

    added release management

--- create_release.sh
+++ create_release.sh
@@ -5,6 +5,6 @@ version=$(python -c "from src.version import version; print(version)")
 echo Create release for version $version
 rm -rf release
 mkdir release
-tar -cf release/cln_super_bitcoind_$version.tar.gz src/ LICENSE README.md requirements.txt super_bitcoind.py test_bitcoin_node_availability.py
+tar -cf release/cln_channel_acceptor_$version.tar.gz src/ LICENSE README.md requirements.txt channel_acceptor.py
 cd release
-shasum -a 256 cln_super_bitcoind_$version.tar.gz > SHA256SUMS
+shasum -a 256 cln_channel_acceptor_$version.tar.gz > SHA256SUMS
