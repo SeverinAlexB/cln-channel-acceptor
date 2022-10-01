1b5320b7a41c2a54388b71caee1dd115beaf4940
Severin Buhler <sevi_buehler@hotmail.com>
2022-10-01 11:51:33 +0300

    added __version__.py got the release

--- create_release.sh
+++ create_release.sh
@@ -5,6 +5,6 @@ version=$(python3 -c "from __version__ import version; print(version)")
 echo Create release for version $version
 rm -rf release
 mkdir release
-tar -cf release/cln_channel_acceptor_$version.tar.gz src/ LICENSE README.md requirements.txt channel_acceptor.py
+tar -cf release/cln_channel_acceptor_$version.tar.gz __version__.py src/ LICENSE README.md requirements.txt channel_acceptor.py
 cd release
 shasum -a 256 cln_channel_acceptor_$version.tar.gz > SHA256SUMS
